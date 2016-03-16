2b3a007108c7e08219cef472d35f2c23710457bf
Anton <anton.yacenko@gmail.com>
2016-03-16 20:14:41 +0200

    highlight active user

--- 2.js
+++ 2.js
@@ -21,12 +21,20 @@ function drawUsers(usersList) {
   });
 }
 
+var selectedUser;
+
 function drawOneUser(userObject) {
   var item = document.createElement('li');
   item.innerHTML = userObject.name;
   item.className = 'user';
   item.onclick = function() {
+    if (selectedUser) {
+      selectedUser.className = 'user';
+    }
+
     document.getElementById('userInfoNode').innerHTML = userObject.info;
+    this.className = 'userActive';
+    selectedUser = this;
   };
 
   usersList.appendChild(item);
