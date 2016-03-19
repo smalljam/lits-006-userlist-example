5f1522997f055d040b8c4c761565dbf90c96b229
iuliiabaranovska <iuliia.baranovska@gmail.com>
2016-03-19 22:34:15 +0200

    using mustache

--- 2.js
+++ 2.js
@@ -4,18 +4,42 @@ var users = [{
     second: 'Second',
     avatar: 'http://www.iconarchive.com/download/i51046/hopstarter/halloween-avatars/Jason.ico',
     newfield: 'asdasdasdasd',
+    skills: ['EcmaScript', 'PHP', 'Java'],
+    fullTime: true,
+    phone: "617-123-4567",
+    address: {
+        city: "Boston",
+        state: "MA",
+        zip: "02106"
+    }
 }, {
     name: 'Pavel',
     info: 'plays footbal',
     second: 'Second',
     avatar: 'http://www.iconarchive.com/download/i51026/hopstarter/halloween-avatars/Alien.ico',
     newfield: 'asdasdasdasd',
+    skills: ['JavaScript', 'C#', 'Paskal'],
+    fullTime: false,
+    phone: "617-987-6543",
+    address: {
+        city: "London",
+        state: "GH",
+        zip: "45676"
+    }
 }, {
     name: 'Igor',
     info: 'drinks a lot',
     second: 'Second',
     avatar: 'http://files.softicons.com/download/internet-cons/halloween-avatars-icons-by-deleket/ico/Zombie%202.ico',
     newfield: 'asdasdasdasd',
+    skills: ['C++', 'Ruby'],
+    fullTime: true,
+    phone: "617-111-2323",
+    address: {
+        city: "Amsterdam",
+        state: "JJ",
+        zip: "69696"
+    }
 }, ];
 
 var usersList = document.getElementById('usersListNode');
@@ -43,7 +67,7 @@ function drawOneUser(userObject, index) {
 
         //by classList
         if (selectedUser) {
-            selectedUser.classList.toggle('userActive',false);
+            selectedUser.classList.toggle('userActive', false);
         }
         setUserInfo(userObject);
         this.classList.toggle('userActive');
@@ -51,21 +75,17 @@ function drawOneUser(userObject, index) {
 
     }
     usersList.appendChild(item);
-
 }
 
-var template = userInfoNode.innerHTML;
+var template = userInfoNode.innerHTML.replace(/{{&gt;/g, "{{>");
+
 setUserInfoNodeHTML('');
 
+var partials = { address: "{{city}}, {{state}} {{zip}}" };
+
 function setUserInfo(userObject) {
-    var newContent = template;
-
-    var fields = Object.keys(userObject);
-    fields.forEach(function(fieldName) {
-        newContent = newContent.replace('{{' + fieldName + '}}', userObject[fieldName]);
-    });
-
-    setUserInfoNodeHTML(newContent);
+    var newContent = Mustache.to_html(template, userObject, partials);
+    userInfoNode.innerHTML = newContent;
 }
 
 function setUserInfoNodeHTML(content) {
