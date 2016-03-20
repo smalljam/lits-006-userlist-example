4e0df0c076d3e2b0f3546c4a2afa858d199bdd8b
vitaliy <vitaliy@globalit.no>
2016-03-20 22:54:53 +0200

    add mustache.js

--- 2.js
+++ 2.js
@@ -1,65 +1,43 @@
-var users = [
-  {
-    name: 'John',
-    info: 'likes bowling',
-    second: 'Second',
-    avatar: 'http://www.iconarchive.com/download/i51046/hopstarter/halloween-avatars/Jason.ico',
-    newfield: 'asdasdasdasd',
-  },
-  {
-    name: 'Pavel',
-    info: 'plays footbal',
-    second: 'Second',
-    avatar: 'http://www.iconarchive.com/download/i51026/hopstarter/halloween-avatars/Alien.ico',
-    newfield: 'asdasdasdasd',
-  },
-  {
-    name: 'Igor',
-    info: 'drinks a lot',
-    second: 'Second',
-    avatar: 'http://files.softicons.com/download/internet-cons/halloween-avatars-icons-by-deleket/ico/Zombie%202.ico',
-    newfield: 'asdasdasdasd',
-  },
-];
+$(function(){
+  loadData().promise().done(function(data) {
+    drawUsers(data.users);
+  });
+});
+
+function loadData() {
+  return $.getJSON('data.json', function(data) {
+    return data.users;
+  });
+}
 
 var usersList = $('#usersListNode');
 var userInfoNode = $('#userInfoNode');
-
-function drawUsers(usersList) {
-  usersList.forEach(drawOneUser);
-}
-
 var selectedUser;
+var template = $('#userInfoTemplate').html();
+
+function drawUsers(users) {
+  users.forEach(drawOneUser);
+}
 
 function drawOneUser(user, index) {
   var item = $(document.createElement('li'));
   item.text(index + ':' + user.name);
   item.addClass('user');
-  item.click(function() {
-    if (selectedUser)
-      selectedUser.removeClass('userActive');
-    setUserInfo(user);
-    var self = $(this);
-    self.addClass('userActive');
-    selectedUser = self;
-  });
+  item.click(function() { clickOnUser(user); });
   usersList.append(item);
 }
 
-var template = userInfoNode.html();
-
-setUserInfoNodeHTML('');
-
-function setUserInfo(user) {
-  var newContent = template;
-  var fields = Object.keys(user);
-  fields.forEach(function(fieldName) {
-    newContent = newContent.replace('{{' + fieldName + '}}', user[fieldName]);
-  });
-
-  setUserInfoNodeHTML(newContent);
+function clickOnUser(user) {
+  if (selectedUser)
+    selectedUser.removeClass('userActive');
+  var self = $(this);
+  self.addClass('userActive');
+  selectedUser = self;
+  setUserInfo(user);
 }
 
-function setUserInfoNodeHTML(content) {
-  userInfoNode.html(content);
+function setUserInfo(user) {
+  var newTemplate = template;
+  var newContent = Mustache.to_html(template, user);
+  userInfoNode.html(newContent);
 }
