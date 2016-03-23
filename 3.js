7e63551bf67b81f09d2d2978a7e645db3052fded
Anton <anton.yacenko@gmail.com>
2016-03-23 20:20:14 +0200

    event delegation

--- 3.js
+++ 3.js
@@ -3,22 +3,29 @@ var r2 = document.getElementById('root2');
 var button = document.getElementById('button');
 var par1 = document.getElementById('par1');
 
-button.addEventListener('click', function(e) {
-  console.log('button clicked');
-  e.stopPropagation();
-});
-
 r1.addEventListener('click', function(e) {
   this.style.height = '20px';
   this.style.overflow = 'hidden';
 });
 
 par1.addEventListener('click', function(e) {
-  console.log('par1 clicked');
+
+  console.log(e.target);
+
+  if (e.target.tagName == 'BUTTON') {
+    console.log(e.target.id, 'clicked');
+  } else {
+    this.style.fontSize = '36px';
+    console.log('par1 clicked');
+  }
+
   e.stopPropagation();
-  document.dispatchEvent(new MouseEvent('click'));
 });
 
 document.addEventListener('click', function(e) {
   console.log('document clicked', e.clientX);
 });
+
+var newButton = document.createElement('button');
+newButton.id = 'button3';
+par1.appendChild(newButton);
