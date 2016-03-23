c6ec8b31216225b111172c582cd8595c787b5e23
Anton <anton.yacenko@gmail.com>
2016-03-23 20:04:28 +0200

    stopPropagation + click simulation

--- 3.js
+++ 3.js
@@ -1,11 +1,24 @@
 var r1 = document.getElementById('root1');
 var r2 = document.getElementById('root2');
 var button = document.getElementById('button');
+var par1 = document.getElementById('par1');
 
 button.addEventListener('click', function(e) {
   console.log('button clicked');
+  e.stopPropagation();
 });
 
 r1.addEventListener('click', function(e) {
-  console.log('r1 clicked');
+  this.style.height = '20px';
+  this.style.overflow = 'hidden';
+});
+
+par1.addEventListener('click', function(e) {
+  console.log('par1 clicked');
+  e.stopPropagation();
+  document.dispatchEvent(new MouseEvent('click'));
+});
+
+document.addEventListener('click', function(e) {
+  console.log('document clicked', e.clientX);
 });
