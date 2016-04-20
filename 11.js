a7b443865a6fed08b10ae75d2c13575695a1ae17
Anton <anton.yacenko@gmail.com>
2016-04-20 19:30:30 +0300

    router example

---
+++ 11.js
@@ -0,0 +1,11 @@
+var router = {
+  index: function(){
+    console.log('index page')
+  },
+  users: function(){
+    console.log('users page')
+  },
+  about: function(){
+    console.log('about page')
+  }
+}
