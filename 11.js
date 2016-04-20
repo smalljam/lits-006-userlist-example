56935cd94d285ffd071c6fad1cb539f3807083db
Anton <anton.yacenko@gmail.com>
2016-04-20 19:37:32 +0300

    index + users

--- 11.js
+++ 11.js
@@ -1,9 +1,24 @@
+var content = document.getElementById('content');
 var router = {
   index: function(){
-    console.log('index page')
+    content.innerHTML = 'INDEX PAGE';
   },
   users: function(){
-    console.log('users page')
+    content.innerHTML = '';
+    var users = getUsers();
+    users.forEach(function(user){
+      content.innerHTML += JSON.stringify(user);
+    });
+
+    function getUsers(){
+      var request = new XMLHttpRequest();
+      request.open('GET', 'http://heedio.me:8383', false);  // `false` makes the request synchronous
+      request.send(null);
+      if (request.status === 200) {
+        usersArray = JSON.parse(request.responseText);
+      }
+      return usersArray;
+    }
   },
   about: function(){
     console.log('about page')
